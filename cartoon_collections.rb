6cd4e0debabd791d7a8cdea9dbeb8e8d2f4ab5af
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-26 01:45:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves_array)
+    dwarves_array.each_with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    n_array = []
+    str = ""
+    planeteer_calls.each do |call|
+      str = call.capitalize + "!"
+      n_array.push(str)
+    end
+    n_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls(calls))
+#     calls.each do |call|
+#       if !call
+#         puts false
+#     end
+#     puts true
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
