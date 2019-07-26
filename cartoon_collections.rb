965048a5f5a1cf7a67c9f529a06c233b7af2545b
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-26 01:58:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,10 @@ def long_planeteer_calls(calls)
     p flag
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+   dwarves_array.each_with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+    end
 end
